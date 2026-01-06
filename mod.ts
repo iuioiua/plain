9aae3cadb619541394921a7243229143d966a773
Asher Gomez <ashersaupingomez@gmail.com>
2026-01-06 22:50:16 +1100

    refactor: consolidate conditionals and remove 499 return in `toHttpErrorCode()`

--- mod.ts
+++ mod.ts
@@ -26,34 +26,30 @@ export class HttpError extends Error {
   }
 }
 
-function toHttpErrorCode(error: Error): ErrorStatus | 499 {
-  if (error instanceof SyntaxError) return 400;
-  if (error instanceof TypeError) return 400;
-  if (error instanceof RangeError) return 400;
-  if (error instanceof URIError) return 400;
-  if (error instanceof EvalError) return 400;
-
-  if (error instanceof Deno.errors.NotFound) return 404;
+function toHttpErrorCode(error: Error): ErrorStatus {
+  if (
+    error instanceof SyntaxError || error instanceof TypeError ||
+    error instanceof RangeError || error instanceof URIError ||
+    error instanceof EvalError || error instanceof Deno.errors.AddrNotAvailable
+  ) return 400;
   if (error instanceof Deno.errors.PermissionDenied) return 403;
+  if (error instanceof Deno.errors.NotFound) return 404;
   if (error instanceof Deno.errors.AlreadyExists) return 409;
   if (error instanceof Deno.errors.InvalidData) return 422;
+  if (
+    error instanceof Deno.errors.ConnectionRefused ||
+    error instanceof Deno.errors.ConnectionReset ||
+    error instanceof Deno.errors.Http ||
+    error instanceof Deno.errors.UnexpectedEof
+  ) return 502;
   if (error instanceof Deno.errors.Busy) return 503;
   if (error instanceof Deno.errors.TimedOut) return 504;
-  if (error instanceof Deno.errors.Interrupted) return 499;
-  if (error instanceof Deno.errors.ConnectionRefused) return 502;
-  if (error instanceof Deno.errors.ConnectionReset) return 502;
-  if (error instanceof Deno.errors.ConnectionAborted) return 499;
-  if (error instanceof Deno.errors.BrokenPipe) return 499;
-  if (error instanceof Deno.errors.AddrNotAvailable) return 400;
-  if (error instanceof Deno.errors.Http) return 502;
-  if (error instanceof Deno.errors.UnexpectedEof) return 502;
-
   return 500;
 }
 
 export function toHttpError(error: Error): HttpError {
   if (error instanceof HttpError) return error;
-  const status = toHttpErrorCode(error) as ErrorStatus;
+  const status = toHttpErrorCode(error);
   return new HttpError(status, error.message, { cause: error });
 }
 
@@ -64,8 +60,10 @@ export function route(
   for (const { pattern, handlers } of routes) {
     const match = pattern.exec(request.url);
     if (!match) continue;
+
     const handler = handlers[request.method as Method];
     if (!handler) throw new HttpError(405, undefined, { cause: request });
+
     return handler(request, match);
   }
   throw new HttpError(404, undefined, { cause: request });
